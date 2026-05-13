1d01b457998f0a5c2bdc4702c511049e5c1cd496
kaztalek <matt.a.pilla@gmail.com>
2026-05-12 19:32:50 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# ggr-analyzer
+> Analyzes replay data in `.ggr` files for Guilty Gear XX Accent Core Plus R
+
+## Prerequisites
+- [Bun](https://bun.com/docs)
+
+## Setup
+Install dependencies:
+```bash
+bun i
+```
+Create a `.env` file based on `.env.example`. It should have a comma-separated list of your replay directories.
+
+## Running
+Run across the replays from the paths specified in `.env`:
+```bash
+bun .
+```
+
+On an individual file:
+```bash
+bun . path/to/replay.ggr
+```
---
+++ constants.ts
@@ -0,0 +1,58 @@
+export const CHARACTERS = [
+	{id: 0, name: 'Sol', code: 'SO'},
+	{id: 1, name: 'Ky', code: 'KY'},
+	{id: 2, name: 'May', code: 'MA'},
+	{id: 3, name: 'Millia', code: 'MI'},
+	{id: 4, name: 'Axl', code: 'AX'},
+	{id: 5, name: 'Potemkin', code: 'PO'},
+	{id: 6, name: 'Chipp', code: 'CH'},
+	{id: 7, name: 'Eddie', code: 'ED'},
+	{id: 8, name: 'Baiken', code: 'BA'},
+	{id: 9, name: 'Faust', code: 'FA'},
+	{id: 10, name: 'Testament', code: 'TE'},
+	{id: 11, name: 'Jam', code: 'JA'},
+	{id: 12, name: 'Anji', code: 'AN'},
+	{id: 13, name: 'Johnny', code: 'JO'},
+	{id: 14, name: 'Venom', code: 'VE'},
+	{id: 15, name: 'Dizzy', code: 'DI'},
+	{id: 16, name: 'Slayer', code: 'SL'},
+	{id: 17, name: 'I-No', code: 'IN'},
+	{id: 18, name: 'Zappa', code: 'ZA'},
+	{id: 19, name: 'Bridget', code: 'BR'},
+	{id: 20, name: 'Robo-Ky', code: 'RO'},
+	{id: 21, name: 'A.B.A', code: 'AB'},
+	{id: 22, name: 'Order-Sol', code: 'OR'},
+	{id: 23, name: 'Kliff', code: 'KL'},
+	{id: 24, name: 'Justice', code: 'JU'}
+];
+
+export const OUTCOMES: {[key: number]: string} = {
+	0: 'OK',
+	1: 'unfinished',
+	2: 'disconnect',
+	4: 'desync'
+};
+
+export const RANKS = [
+	'0: Civilian',
+	'1: Cadet',
+	'2: Bodyguard',
+	'3: Enlistee',
+	'4: Low-ranking Soldier',
+	'5: High-ranking Soldier',
+	'6: Novice Squadsmen',
+	'7: Squadsman',
+	'8: Senior Squadsman',
+	'9: Platoon Leader',
+	'10: Battalion Leader',
+	'11: Champion',
+	'12: Master',
+	'13: Holy Knight',
+	'14: Holy Knight Commander',
+	'15: Master Swordsman',
+	'16: Dragon Hunter',
+	'17: Titan',
+	'18: Hero',
+	'19: War God',
+	'20: Legend'
+];
