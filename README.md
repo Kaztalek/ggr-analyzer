e42f082019cccc086ce85418b9b16da7c0ce5775
kaztalek <matt.a.pilla@gmail.com>
2026-05-17 23:35:10 -0500

    add character density report

--- README.md
+++ README.md
@@ -12,11 +12,17 @@ bun i
 Create a `.env` file based on `.env.example`. It should have a comma-separated list of your replay directories.
 
 ## Running
+
+### All replays
 Run across the replays from the paths specified in `.env`:
 ```bash
 bun .
 ```
 
+#### Output
+`reports/character-distribution.csv`: Ranks how many unique opponents you've faced for each character, and your win rate against them
+
+### Individual replay
 Print stats on an individual replay file:
 ```bash
 bun . path/to/replay.ggr
--- reports.ts
+++ reports.ts
@@ -1,5 +1,79 @@
+import {CHARACTERS} from './constants';
 import {type ggrReplayType} from './replayUtils';
+import {json2csv} from 'json-2-csv';
+import {writeFile} from 'fs/promises';
 
-export const generateCharacterDistribution = (replays: ggrReplayType[]) => {
-	console.log('OK');
+const STEAM_ID = process.env.STEAM_ID;
+
+type charDistributionDataType = {
+	total: number;
+	unique: Set<string>;
+	wins: number;
+	yours: number;
+};
+type charDistributionFieldType = {
+	Character: string;
+	['Unique Opponents']: number;
+	['Total Matches']: number;
+	Wins: number;
+	['Win Rate']: string;
+	['Matches Played As']: number;
+};
+
+export const generateCharacterDistribution = async (
+	replays: ggrReplayType[]
+) => {
+	const charData: {[key: string]: charDistributionDataType} = {};
+	CHARACTERS.forEach((char) => {
+		charData[char.code] = {total: 0, unique: new Set(), wins: 0, yours: 0};
+	});
+
+	replays.forEach((replay) => {
+		// skip replays that aren't normal 1v1 matches
+		if (
+			replay.modifiedOptions ||
+			replay.mode !== 'single' ||
+			replay.errors.length
+		) {
+			return;
+		}
+		const isPlayer1 = STEAM_ID === replay.p1SteamId;
+		// skip replays where you are not playing
+		if (!isPlayer1 && STEAM_ID !== replay.p2SteamId) {
+			return;
+		}
+
+		const charCode = isPlayer1 ? replay.p1Char.code : replay.p2Char.code;
+		const oppCharCode = isPlayer1 ? replay.p2Char.code : replay.p1Char.code;
+		const oppId = isPlayer1 ? replay.p2SteamId : replay.p1SteamId;
+		const didWin =
+			(isPlayer1 && replay.winner === 'P1') ||
+			(!isPlayer1 && replay.winner === 'P2');
+
+		charData[oppCharCode].total += 1;
+		charData[oppCharCode].unique.add(oppId);
+		charData[oppCharCode].wins += didWin ? 1 : 0;
+		charData[charCode].yours += 1;
+	});
+
+	const csvData: charDistributionFieldType[] = [];
+	CHARACTERS.forEach((char) => {
+		const stats = charData[char.code];
+		csvData.push({
+			Character: char.name,
+			['Unique Opponents']: stats.unique.size,
+			['Total Matches']: stats.total,
+			Wins: stats.wins,
+			['Win Rate']: `${((stats.wins / stats.total) * 100).toFixed(1)}%`,
+			['Matches Played As']: stats.yours
+		});
+	});
+	const results = csvData.sort(
+		(a: charDistributionFieldType, b: charDistributionFieldType) => b['Unique Opponents'] - a['Unique Opponents']
+	);
+
+	const csv = await json2csv(results, {});
+	const outputFilepath = './reports/character-distribution.csv';
+	await writeFile(outputFilepath, csv);
+	console.log(`Wrote ${outputFilepath}`);
 };
