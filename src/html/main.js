7eea29da57a7d0fe58f2be7c828a83b49935d16a
kaztalek <matt.a.pilla@gmail.com>
2026-09-07 17:48:25 -0500

    chart all characters

--- src/html/main.js
+++ src/html/main.js
@@ -1,38 +1,41 @@
 const {createApp, ref} = Vue;
 
-const CHARACTERS = [
-	{id: 1, name: 'Sol', code: 'SO'},
-	{id: 2, name: 'Ky', code: 'KY'},
-	{id: 3, name: 'May', code: 'MA'},
-	{id: 4, name: 'Millia', code: 'MI'},
-	{id: 5, name: 'Axl', code: 'AX'},
-	{id: 6, name: 'Potemkin', code: 'PO'},
-	{id: 7, name: 'Chipp', code: 'CH'},
-	{id: 8, name: 'Eddie', code: 'ED'},
-	{id: 9, name: 'Baiken', code: 'BA'},
-	{id: 10, name: 'Faust', code: 'FA'},
-	{id: 11, name: 'Testament', code: 'TE'},
-	{id: 12, name: 'Jam', code: 'JA'},
-	{id: 13, name: 'Anji', code: 'AN'},
-	{id: 14, name: 'Johnny', code: 'JO'},
-	{id: 15, name: 'Venom', code: 'VE'},
-	{id: 16, name: 'Dizzy', code: 'DI'},
-	{id: 17, name: 'Slayer', code: 'SL'},
-	{id: 18, name: 'I-No', code: 'IN'},
-	{id: 19, name: 'Zappa', code: 'ZA'},
-	{id: 20, name: 'Bridget', code: 'BR'},
-	{id: 21, name: 'Robo-Ky', code: 'RO'},
-	{id: 22, name: 'A.B.A', code: 'AB'},
-	{id: 23, name: 'Order-Sol', code: 'OR'},
-	{id: 24, name: 'Kliff', code: 'KL'},
-	{id: 25, name: 'Justice', code: 'JU'}
-];
+// dominant color gotten from parsing icons here: https://lokeshdhakar.com/projects/color-thief/
+// used 2nd dominant color for Sol to differentiate SO and OR
+const CHARACTERS = {
+	SO: {name: 'Sol', code: 'SO', color: '#9a3e2f'},
+	KY: {name: 'Ky', code: 'KY', color: '#fbc065'},
+	MA: {name: 'May', code: 'MA', color: '#de551e'},
+	MI: {name: 'Millia', code: 'MI', color: '#f5ce88'},
+	AX: {name: 'Axl', code: 'AX', color: '#962437'},
+	PO: {name: 'Potemkin', code: 'PO', color: '#341307'},
+	CH: {name: 'Chipp', code: 'CH', color: '#aa9daf'},
+	ED: {name: 'Eddie', code: 'ED', color: '#cc9e7c'},
+	BA: {name: 'Baiken', code: 'BA', color: '#8c201a'},
+	FA: {name: 'Faust', code: 'FA', color: '#b9764c'},
+	TE: {name: 'Testament', code: 'TE', color: '#14131c'},
+	JA: {name: 'Jam', code: 'JA', color: '#692d15'},
+	AN: {name: 'Anji', code: 'AN', color: '#462e1f'},
+	JO: {name: 'Johnny', code: 'JO', color: '#442d2c'},
+	VE: {name: 'Venom', code: 'VE', color: '#173698'},
+	DI: {name: 'Dizzy', code: 'DI', color: '#31526b'},
+	SL: {name: 'Slayer', code: 'SL', color: '#501600'},
+	IN: {name: 'I-No', code: 'IN', color: '#5b0016'},
+	ZA: {name: 'Zappa', code: 'ZA', color: '#52291b'},
+	BR: {name: 'Bridget', code: 'BR', color: '#313b65'},
+	RO: {name: 'Robo-Ky', code: 'RO', color: '#fac37a'},
+	AB: {name: 'A.B.A', code: 'AB', color: '#882f0b'},
+	OR: {name: 'Order-Sol', code: 'OR', color: '#3f1e1f'},
+	KL: {name: 'Kliff', code: 'KL', color: '#908266'},
+	JU: {name: 'Justice', code: 'JU', color: '#b67a5f'}
+};
+const CHARACTER_DATA = Object.values(CHARACTERS);
 
 const getCharacterData = () => {
 	const charReplayTotals = {};
-	CHARACTERS.forEach((char) => (charReplayTotals[char.code] = 0));
+	CHARACTER_DATA.forEach((char) => (charReplayTotals[char.code] = 0));
 	replayData.forEach((replay) => (charReplayTotals[replay.charCode] += 1));
-	return CHARACTERS.map((char) => ({
+	return CHARACTER_DATA.map((char) => ({
 		...char,
 		replayTotal: charReplayTotals[char.code]
 	})).sort((a, b) => b.replayTotal - a.replayTotal);
@@ -54,51 +57,101 @@ createApp({
 }).mount('#app');
 
 // TODO handle case where replayData doesn't exist
-const chartData = [];
-let totalReplays = 0;
+
 let wins = 0;
-replayData.forEach((replay) => {
-	totalReplays += 1;
-	wins += replay.didWin ? 1 : 0;
-	chartData.push({
-		x: replay.date,
-		y: ((wins / totalReplays) * 100).toFixed(1)
-	});
+const cumulativeData = replayData.map((replay, i) => {
+	if (replay.didWin) {
+		wins += 1;
+	}
+
+	return {
+		...replay,
+		x: i + 1,
+		y: ((wins / (i + 1)) * 100).toFixed(1)
+	};
 });
+
+const datasets = [
+	{
+		data: cumulativeData,
+		label: 'All characters',
+		borderColor: '#7bedd4',
+		backgroundColor: '#7bedd4cc' // TODO redundant code
+	},
+	...Object.values(
+		replayData.reduce((charSets, replay) => {
+			const charSet = (charSets[replay.charCode] ??= {
+				data: [],
+				label: replay.charCode,
+				hidden: true,
+				wins: 0,
+				borderColor: CHARACTERS[replay.charCode].color,
+				backgroundColor: `${CHARACTERS[replay.charCode].color}cc`
+			});
+
+			if (replay.didWin) {
+				charSet.wins += 1;
+			}
+
+			charSet.data.push({
+				...replay,
+				x: charSet.data.length + 1,
+				y: ((charSet.wins / (charSet.data.length + 1)) * 100).toFixed(1)
+			});
+
+			return charSets;
+		}, {})
+	).sort((a, b) => b.data.length - a.data.length)
+];
+
+// TODO move to function
+// TODO calculate new running percentage
+// filter to last 100 games
+const filteredDatasets = datasets.map((dataset) => ({
+	...dataset,
+	data: dataset.data.slice(-100).map((replay, i) => ({
+		...replay,
+		x: i + 1
+	}))
+}));
 
 const CHART = new Chart(document.getElementById('chart'), {
 	type: 'line',
 	data: {
-		datasets: [
-			{
-				label: 'Win Rate (%)',
-				data: chartData
-			}
-		]
+		datasets: filteredDatasets
 	},
 	options: {
 		scales: {
 			x: {
-				type: 'time',
-				time: {
-					tooltipFormat: 'YYYY-MM-DD HH:mm:ss',
-					displayFormats: {
-						millisecond: 'MMM DD YYYY ha',
-						seconds: 'MMM DD YYYY ha',
-						minute: 'MMM DD YYYY ha',
-						hour: 'MMM DD YYYY ha',
-						day: 'MMM DD YYYY',
-						week: 'MMM DD YYYY',
-						month: 'MMM YYYY',
-						quarter: 'MMM YYYY',
-						year: 'YYYY'
-					}
-				},
+				type: 'linear',
+				min: 1,
 				title: {
 					display: true,
-					text: 'Date'
+					text: 'Last 100 games'
 				}
 			},
+			// TODO have time-scaled x-axis option in the future
+			// x: {
+			// 	type: 'time',
+			// 	time: {
+			// 		tooltipFormat: 'YYYY-MM-DD HH:mm:ss',
+			// 		displayFormats: {
+			// 			millisecond: 'MMM DD YYYY ha',
+			// 			seconds: 'MMM DD YYYY ha',
+			// 			minute: 'MMM DD YYYY ha',
+			// 			hour: 'MMM DD YYYY ha',
+			// 			day: 'MMM DD YYYY',
+			// 			week: 'MMM DD YYYY',
+			// 			month: 'MMM YYYY',
+			// 			quarter: 'MMM YYYY',
+			// 			year: 'YYYY'
+			// 		}
+			// 	},
+			// 	title: {
+			// 		display: true,
+			// 		text: 'Date'
+			// 	}
+			// },
 			y: {
 				beginAtZero: true,
 				title: {
@@ -108,6 +161,16 @@ const CHART = new Chart(document.getElementById('chart'), {
 			}
 		},
 		plugins: {
+			tooltip: {
+				callbacks: {
+					title: (items) =>
+						moment(items[0].dataset.data[items[0].dataIndex].date).format(
+							'YYYY-MM-DD HH:mm:ss'
+						),
+					label: (context) =>
+						`${CHARACTERS[context.raw.charCode]?.name ?? context.raw.charCode}: ${context.formattedValue}%`
+				}
+			},
 			zoom: {
 				pan: {
 					enabled: true,
